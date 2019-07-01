0aa05e084041f132ae608e6263a039022bf3d0b8
Will Bush <williamdcbush@gmail.com>
2019-07-01 16:13:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_fourth_tier
 monopoly = {:railroads => {
 	  :pieces => 4,
 	  :names => {
-	    :reading_railroad => {'mortgage value' => '$100'},
+	    :reading_railroad => {'mortgage_value' => '$100'},
 	    :pennsylvania_railroad => {},
 	    :b_and_o_railroad => {},
 	    :shortline => {}
