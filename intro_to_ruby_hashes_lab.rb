c16c3b1397a6ede53d2e778fda7fc6dc8bd9f767
Will Bush <williamdcbush@gmail.com>
2019-07-01 16:13:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ monopoly = {:railroads => {
 	  :pieces => 4,
 	  :names => {
 	    :reading_railroad => {'mortgage_value' => '$100'},
-	    :pennsylvania_railroad => {},
+	    :pennsylvania_railroad => {'mortgage_value' => '$200'},
 	    :b_and_o_railroad => {},
 	    :shortline => {}
 	    
