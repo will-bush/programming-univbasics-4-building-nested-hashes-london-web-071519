2f65c6014da334266e9bade1f10855faf6026694
Will Bush <williamdcbush@gmail.com>
2019-07-01 15:59:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,10 @@ def monopoly_with_second_tier
 	monopoly = {:railroads => {
 	  :pieces => 4,
 	  :names => {},
-	  :rent_in_dollars => {}
+	  :rent_in_dollars => {
+	    :one_piece_owned => 25,
+	    :two_pices_owned => 50
+	  }
 	  
 	}
 	  
